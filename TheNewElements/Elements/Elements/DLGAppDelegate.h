a82a548b173322023d61d44deae2bcfe4cba2f83
DAloG <dalog@me.com>
2013-10-21 20:11:55 +0300

    Add new empty project.
    Add Empty project as stup for alternative elements project.

---
+++ TheNewElements/Elements/Elements/DLGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DLGAppDelegate.h
+//  Elements
+//
+//  Created by Алексей Демедецкий on 21.10.13.
+//  Copyright (c) 2013 dalog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DLGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TheNewElements/Elements/Elements/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Elements
+//
+//  Created by Алексей Демедецкий on 21.10.13.
+//  Copyright (c) 2013 dalog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DLGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DLGAppDelegate class]));
+    }
+}
